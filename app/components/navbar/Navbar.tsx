47e1b9fb692b467c97aa329e12b6222bea069c73
Mohammed-Shaheer-23 <shaheersattr23@gmail.com>
2024-12-29 17:25:33 +0000

    Update Navbar and add Categories components

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
@@ -39,9 +40,8 @@ const Navbar: React.FC<NavbarProps> = ({
                 <UserMenu currentUser={currentUser} />
             </div>
         </Container>
-
-            </div>
-            
+            </div>  
+            <Categories  /> 
         </div>
     );
 }
